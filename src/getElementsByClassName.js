981967300405ba6de337783852bb0c3d0c86cfe5
dclay1983 <dclay1983@gmail.com>
2019-10-21 12:04:12 -0700

    Factor to pass

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,17 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, element=document.body, results=[]) {
+
+  if (element.firstElementChild) {
+    getElementsByClassName(className, element.firstElementChild, results);
+  }
+  if(element.nextElementSibling) {
+    getElementsByClassName(className, element.nextElementSibling, results);
+  }
+  className = new RegExp("(^|\\s)" + className + "(\\s|$)");
+  if(className.test(element.getAttribute("class"))) {
+    results.unshift(element);
+  }
+  return results;
 };
